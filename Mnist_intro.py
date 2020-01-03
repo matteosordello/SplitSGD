afa0302bb3374bd64055c56057cdc4813a33db0c
Matteo Sordello <sordello@wharton.upenn.edu>
2020-01-03 10:44:53 +0100

    Update and rename Mnist.py to Mnist_intro.py

--- Mnist_intro.py
+++ Mnist_intro.py
@@ -16,7 +16,8 @@ batch_size = 100
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-'''
+# Feedforward network with 1 hidden layer
+
 class FeedforwardNeuralNetModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedforwardNeuralNetModel, self).__init__()
@@ -28,7 +29,30 @@ class FeedforwardNeuralNetModel(nn.Module):
         out = self.sigmoid(out)
         out = self.fc2(out)
         return out
-'''
+      
+''' 
+# Feedforward network with 3 hidden layers
+
+class FeedforwardNeuralNetModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(FeedforwardNeuralNetModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim) 
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(hidden_dim, output_dim)  
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        out = self.relu2(out)
+        out = self.fc3(out)
+        out = self.relu3(out)
+        out = self.fc4(out)
+        return out
+ '''
     
 input_dim = 28*28
 hidden_dim = 100
